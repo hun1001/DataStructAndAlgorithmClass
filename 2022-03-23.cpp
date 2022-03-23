7ae48baa1e8cf5ae24e22f41178dee34786d98b7
hun1001 <l01022899266@gmail.com>
2022-03-23 11:02:48 +0900

    다음 수업 전까지 파스칼의 삼각형 3개 다 완성해야됨

--- 2022-03-23.cpp
+++ 2022-03-23.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void P2(int arr[100]);
+void P3(int arr[100]);
+void PrintArr(int arr[100]);
+
 int main() {
 	int arr[100] = { 0, };
-
+	int i1, i2;
 	for (int i = 0; i < 100; i++) {
-		if (i % 10 == 0) {
-			arr[i] = 1;
-		}
-		else if (i >= 11) {
-			arr[i] = arr[i - 10] + arr[i - 11];
-		}
+		i1 = i / 10;
+		i2 = i % 10;
+		cout << i1 * 10 + i2 << " ";
 	}
+	PrintArr(arr);
+}
 
-	// 배열 프린트해주는 코드
+void PrintArr(int arr[100]) {
 	for (int i = 0; i < 100; i++) {
 		if (arr[i] != 0) {
 			cout << arr[i] << " ";
@@ -22,5 +25,29 @@ int main() {
 			cout << endl;
 		}
 	}
+}
 
+void P3(int arr[100]) {
+	int index = 0, indey = 0;
+	for (int i = 0; i < 100; i++) {
+		indey = ((i / 10) * 10);
+		index = (i % 10);
+		if (index == 0) {
+			arr[i] = 1;
+		}
+		else if (i >= 11) {
+			arr[i] = arr[(indey-1)*10 + index - 1] + arr[((indey - 1) * 10) + indey];
+		}
+	}
+}
+
+void P2(int arr[100]) {
+	for (int i = 0; i < 100; i++) {
+		if (i % 10 == 0) {
+			arr[i] = 1;
+		}
+		else if (i >= 11) {
+			arr[i] = arr[i - 10] + arr[i - 11];
+		}
+	}
 }
