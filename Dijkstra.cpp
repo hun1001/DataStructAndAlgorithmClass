1eaf6efeff0f7047b14319d5d8eea6c65087633c
hun1001 <l01022899266@gmail.com>
2022-09-26 13:29:07 +0900

    상빈코드로 변경

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define MAX_VTXS 255
+#define INF 9999999
 
 using namespace std;
 
@@ -55,82 +56,88 @@ public:
 
 class shortestPath : public AdjMatGraph
 {
-	int dist[MAX_VTXS];
-	bool found[MAX_VTXS];
+    int dist[MAX_VTXS];
+    bool found[MAX_VTXS];
+
 public:
-	void printDistance()
-	{
-		for (int i = 0; i < size; ++i)
-		{
-			cout << dist[i] << " ";
-		}
-		cout << endl;
-	}
+    void PrintDistance()
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            cout << dist[i] << " ";
+        }
+        cout << endl;
+    }
 
-	int chooseVertex()
-	{
-		int min = INFINITY;
-		int minpos = -1;
-		for (int i = 0; i < size; ++i)
-		{
-			if (dist[i] > min && !found[i])
-			{
-				min = dist[i];
-				minpos = i;
-			}
-		}
-		return minpos;
-	}
+    void insertEdge(int u, int v, int w)
+    {
+		setEdge(u, v, w);
+		setEdge(v, u, w);
+    }
 
-	void insertEdge(int i, int j, int v)
-	{
-		AdjMatGraph::setEdge(i, j, v);
-		AdjMatGraph::setEdge(j, i, v);
-	}
+    int ChooseVertex()
+    {
+        int min = INF;
+        int minpos = -1;
+        for (int i = 0; i < size; ++i)
+        {
+            if (dist[i] < min && !found[i])
+            {
+                min = dist[i];
+                minpos = i;
+            }
+        }
+        return minpos;
+    }
 
-	void dijikstra(int start)
-	{
-		for (int i = 0; i < size; ++i)
-		{
-			dist[i] = getEdge(start, i);
-			found[i] = false;
-		}
-		found[start] = true;
-		dist[start] = 0;
-		for (int i = 0; i < size; ++i)
-		{
-			cout << "Step " << i + 1 << " : ";
-			printDistance();
-			int u = chooseVertex();
-			found[u] = true;
-			for (int w = 0; w < size; ++w)
-			{
-				if (!found[w] && dist[u] < dist[w] + getEdge(w, u))
-				{
-					dist[w] = dist[u] + getEdge(w, u);
-				}
-			}
-		}
-	};
+    void dijikstra(int start)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            dist[i] = getEdge(start, i);
+            found[i] = false;
+        }
+        found[start] = true;
+        dist[start] = 0;
+
+        for (int i = 0; i < size; ++i)
+        {
+            cout << "Step " << i + 1 << " : ";
+            PrintDistance();
+            int u = ChooseVertex();
+            found[u] = true;
+
+            for (int w = 0; w < size; ++w)
+            {
+                if (!found[w] && dist[w] > dist[u] + getEdge(w, u))
+                {
+                    dist[w] = dist[u] + getEdge(w, u);
+                }
+            }
+        }
+    }
 };
+
 
 int main()
 {
-	shortestPath g;
-	for (int i = 0; i < 7; ++i)
-	{
-		g.insertVertex('A' + i);
-	}
-	g.insertEdge(0, 1, 2);
-	g.insertEdge(0, 2, 7);
-	g.insertEdge(1, 2, 1);
-	g.insertEdge(1, 3, 3);
-	g.insertEdge(2, 4, 4);
-	g.insertEdge(2, 5, 5);
-	g.insertEdge(3, 2, 2);
-	g.insertEdge(3, 4, 3);
-	g.insertEdge(4, 5, 4);
-	g.insertEdge(4, 6, 1);
-	g.insertEdge(5, 6, 5);
-	g.dijikstra(0);
+    shortestPath g;
+    for (int i = 0; i < 7; ++i)
+    {
+        g.insertVertex('A' + 1);
+    }
+    g.insertEdge(0, 1, 2);
+    g.insertEdge(0, 2, 7);
+    g.insertEdge(1, 2, 1);
+    g.insertEdge(1, 3, 3);
+    g.insertEdge(2, 4, 4);
+    g.insertEdge(2, 5, 5);
+    g.insertEdge(3, 2, 2);
+    g.insertEdge(3, 4, 3);
+    g.insertEdge(4, 5, 4);
+    g.insertEdge(4, 6, 1);
+    g.insertEdge(5, 6, 5);
+
+    g.dijikstra(0);
+
 }
