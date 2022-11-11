11d5d72f8d854ff937fa92fb5d7dc34b23490f55
hun1001 <l01022899266@gmail.com>
2022-11-11 09:56:59 +0900

    문제 품

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,8 +1,61 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int N, M;
+int N_arr[100000] = {};
+int M_arr[100000] = {};
+
+void MergeSort(int arr[], int left, int right)
+{
+	// 모르겠다
+}
+
+int BinarySearch(int arr[], int left, int right, int target)
+{
+	int mid;
+
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+
+		if (arr[mid] == target)
+		{
+			return 1;
+		}
+		else if (arr[mid] > target)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+
+	return 0;
+}
+
 int main()
 {
-	
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> N_arr[i];
+	}
+
+	cin >> M;
+	for (int i = 0; i < M; ++i)
+	{
+		cin >> M_arr[i];
+	}
+
+	//MergeSort(N_arr, 0, N - 1);
+	sort(N_arr, N_arr + N);
+
+	for (int i = 0; i < M; ++i)
+	{
+		cout << BinarySearch(N_arr, 0, N - 1, M_arr[i]) << "\n";
+	}
 }
